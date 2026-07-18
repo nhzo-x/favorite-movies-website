10053e02ad2f69b409cedd2923296d88c79c64a3
nhzo-x <pagen503@gmail.com>
2026-07-17 21:32:18 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const title = document.querySelector("h1");
+
+title.addEventListener("click", function() {
+    title.textContent = "You found my hidden message! Thanks for visting my website! This is my first one, so please excuse any glitches or bugs!"
+});
+
+
+const ratings = document.querySelectorAll(".rating");
+ratings.forEach(function(rating) {
+    const stars = rating.querySelectorAll(".star");
+    stars.forEach(function(star) {
+        star.addEventListener("click",function() {
+            const value = this.dataset.value;
+            stars.forEach(function(s) {
+                if (s.dataset.value<=value) {
+                    s.style.color="gold";
+                } else {
+                    s.style.color = "gray";
+                    
+                }
+               
+          
+            });
+        });
+    });
+
+});
