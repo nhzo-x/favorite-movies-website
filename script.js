97646f5859f037f9e6f4d32d9d5ee5945bfecb94
nhzo-x <pagen503@gmail.com>
2026-07-21 14:06:08 -0600

    Refactor script.js with movie and loading messages

--- script.js
+++ script.js
@@ -25,3 +25,48 @@ ratings.forEach(function(rating) {
     });
 
 });
+
+const movies =[
+    "The Breakfast Club",
+    "Friday",
+    "Beverly Hills Cop",
+    "To Wong Foo, Thanks for Everything! Julie Newmar",
+    "Romy and Michele's High School Reunion",
+    "Scarface",
+    "The Conjuring",
+    "My Cousin Vinny",
+    "Bring It On",
+    "Superstar",
+    "The Truman Show",
+    "Scream",
+    "Austin Powers",
+    "Ferris Bueller's Day Off",
+    "Get Out",
+    "Black Panther",
+    "Michael"
+];
+
+const loadingMessage = [
+    "Say Hello to my Loading Screen!",
+    "Life moves pretty fast.. Except for this loading page.",
+    "Sincerely yours... The Loading Screen. ",
+    "Who you gonna call? Load-faster!"
+];
+
+const button = document.getElementById("movieButton");
+button.addEventListener("click", function(){
+let randomMessage = loadingMessage[Math.floor(Math.random() * loadingMessage.length)];
+document.getElementById("loadingMessage").textContent = randomMessage;
+document.getElementById("loadingMessage").style.animation = "fadeIn 0.8s ease";
+
+setTimeout(function(){
+
+    let randomMovie = movies[Math.floor(Math.random() * movies.length)];
+
+document.getElementById("movieResult").textContent = 
+"Tonight you should watch: "+ randomMovie;
+document.getElementById("movieResult").style.animation ="fadeIn 0.8s ease";
+
+}, 2000);
+
+});
